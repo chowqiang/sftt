4c2e0d9da2121340eb66683c574e55945d545636
choumin <514762755@qq.com>
2020-11-08 10:36:45 +0800

    update lib impl

--- src/stack.c
+++ src/stack.c
@@ -1,6 +1,22 @@
 #include <stdlib.h>
 #include <assert.h>
 #include "stack.h"
+#include "dlist.h"
+
+stack *stack_create(void (*destroy)(void *data)) {
+	stack *s = (stack *)malloc(sizeof(stack));
+	if (s == NULL) {
+		return NULL;
+	}
+
+	s->list = dlist_create(destroy);
+	if (s->list == NULL) {
+		free(s);
+		return NULL;
+	}
+
+	return s;
+}
 
 void stack_init(stack *s, void (*destroy) (void *data)) {
 	assert(s != NULL);
@@ -37,3 +53,22 @@ int stack_size(const stack *s) {
 
 	return dlist_size(s->list);
 }
+
+#if 1
+int main(void) {
+	stack *s = stack_create(NULL);
+	stack_push(s, (void *)1);
+	stack_push(s, (void *)2);
+	stack_push(s, (void *)3);
+
+	int data = 0;
+	stack_pop(s, (void **)&data);
+	printf("%d\n", data);
+	stack_pop(s, (void **)&data);
+	printf("%d\n", data);
+	stack_pop(s, (void **)&data);
+	printf("%d\n", data);
+
+	return 0;
+}
+#endif
