72f6e5fb06fc1ca31ee061ad26baea1a56cccbd9
zhoumin <zhoumin@loongson.cn>
2020-11-17 19:47:44 +0800

    update lib impl

---
+++ src/encode.c
@@ -0,0 +1,42 @@
+#include <stdbool.h>
+#include "compress.h"
+#include "encrypt.h"
+#include "encode.h"
+
+int sftt_buffer_encode(unsigned char *input, int len, unsigned char *output, bool zip, bool crypt) {
+	if (!zip && !crypt) {
+		memcpy(output, input, len);
+		return len;
+	}
+
+	int zip_len = len;
+	if (zip) {
+		zip_len = huffman_compress(input, len, output);
+	}
+
+	if (crypt) {
+		sftt_encrypt_func(output, zip_len);
+	}
+
+	return zip_len;
+
+}
+
+int sftt_buffer_decode(unsigned char *input, int len, unsigned char *output, bool unzip, bool decrypt) {
+	if (!unzip && !decrypt) {
+		memcpy(output, input, len);
+		return len;
+	}
+
+	int unzip_len = len;
+	if (unzip) {
+		unzip_len = huffman_decompress(input, output);
+	}
+
+	if (decrypt) {
+		sftt_decrypt_func(output, unzip_len);
+	}
+
+	return unzip_len;
+
+}
--- src/net_trans.c
+++ src/net_trans.c
@@ -13,13 +13,15 @@
 #include "compress.h"
 
 sftt_packet *malloc_sftt_packet(int block_size) {
-	sftt_packet *sp = (sftt_packet *)malloc(sizeof(sftt_packet));
+	memory_pool *mp = get_singleton_mp();
+
+	sftt_packet *sp = (sftt_packet *)mp_malloc(mp, sizeof(sftt_packet));
 	if (sp == NULL) {
 		return NULL;
 	}	
 	memset(sp, 0, sizeof(*sp));
 
-	sp->content = (unsigned char *)malloc(sizeof(unsigned char) * block_size);
+	sp->content = (unsigned char *)mp_malloc(mp, sizeof(unsigned char) * block_size);
 	if (sp->content == NULL) {
 		free(sp);
 		return NULL;
@@ -29,22 +31,14 @@ sftt_packet *malloc_sftt_packet(int block_size) {
 	return sp;
 }
 
-int sftt_packet_encode(unsigned char *input, int len, unsigned char *output, bool zip, bool crypt) {
-	if (!zip && !crypt) {
-		memcpy(output, input, len);
-		return len;
-	}
-
-	int zip_len = len;
-	if (zip) {
-		zip_len = huffman_compress(input, len, output);
-	}
-
-	if (crypt) {
-		sftt_encrypt_func(output, zip_len);
-	}
-
-	return zip_len;
+int sftt_packet_encode(sftt_packet *src, sftt_packet *dst) {
+	//
+ 	// 1. compress and encrypt content
+	// 2. update content len in header
+ 	//
+	dst->data_len = sftt_buffer_encode(src->content, src->data_len, dst->content, true, true);
+	
+	return 0;
 }
 
 void sftt_packet_send_header(int sock, sftt_packet *sp) {
@@ -54,49 +48,41 @@ void sftt_packet_send_header(int sock, sftt_packet *sp) {
 	// fill header
 	strcpy(header, sp->type);
 	memcpy(header + BLOCK_TYPE_SIZE, &(sp->data_len), PACKET_LEN_SIZE);
-
+	
 	memory_pool *mp = get_singleton_mp();
 	unsigned char *buffer = mp_malloc(mp, header_len);
 
-	int encoded_len = sftt_packet_encode(header, header_len, buffer, false, true);
+	int encoded_len = sftt_buffer_encode(header, header_len, buffer, false, true); 
 	int ret = send(sock, buffer, encoded_len, 0);
 
 	mp_free(mp, buffer);
 }
 
 void sftt_packet_send_content(int sock, sftt_packet *sp) {
-	memory_pool *mp = get_singleton_mp();
-	unsigned char *buffer = mp_malloc(mp, HUFFMAN_META_DATA_SIZE + sp->data_len);
-
-	int encoded_len = sftt_packet_encode(sp->content, sp->data_len, buffer, true, true);
-	int ret = send(sock, buffer, encoded_len, 0);
-
-	mp_free(mp, buffer);
+	int ret = send(sock, sp->content, sp->data_len, 0);
 }
 
 int send_sftt_packet(int sock, sftt_packet *sp) {
-	sftt_packet_send_header(sock, sp);
-	sftt_packet_send_content(sock, sp);
+	sftt_packet *_sp = malloc_sftt_packet(sp->block_size * 2);
+
+	sftt_packet_encode(sp, _sp);
+
+	sftt_packet_send_header(sock, _sp);
+	sftt_packet_send_content(sock, _sp);
+
+	free_sftt_packet(&_sp);
 
 	return 0;
 }
 
-int sftt_packet_decode(unsigned char *input, int len, unsigned char *output, bool unzip, bool decrypt) {
-	if (!unzip && !decrypt) {
-		memcpy(output, input, len);
-		return len;
-	}
+int sftt_packet_decode(sftt_packet *src, sftt_packet *dst) {
+	//
+ 	// 1. decompress and decrypt content
+	// 2. update content len in header
+ 	//
+ 	dst->data_len = sftt_buffer_decode(src->content, src->data_len, dst->content, true, true);
 
-	int unzip_len = len;
-	if (unzip) {
-		unzip_len = huffman_decompress(input, output);
-	}
-
-	if (decrypt) {
-		sftt_decrypt_func(output, unzip_len);
-	}
-
-	return unzip_len;
+	return 0;
 }
 
 int sftt_packet_recv_header(int sock, sftt_packet *sp) {
@@ -109,14 +95,16 @@ int sftt_packet_recv_header(int sock, sftt_packet *sp) {
 	memory_pool *mp = get_singleton_mp();
 	unsigned char *buffer = mp_malloc(mp, header_len);
 
-	int decoded_len = sftt_packet_decode(header, header_len, buffer, false, true);
+	int decoded_len = sftt_buffer_decode(header, header_len, buffer, false, true);
 	assert(decoded_len == header_len);
 
 	memcpy(sp->type, buffer, BLOCK_TYPE_SIZE);
 	sp->type[BLOCK_TYPE_SIZE] = 0;
-        memcpy(&(sp->data_len), buffer + BLOCK_TYPE_SIZE, PACKET_LEN_SIZE);
+    memcpy(&(sp->data_len), buffer + BLOCK_TYPE_SIZE, PACKET_LEN_SIZE);
 
 	mp_free(mp, buffer);
+
+	return 0;
 }
 
 int sftt_packet_recv_content(int sock, sftt_packet *sp) {
@@ -124,31 +112,32 @@ int sftt_packet_recv_content(int sock, sftt_packet *sp) {
 	int ret = recv(sock, sp->content, sp->data_len, 0);
 	assert(ret == sp->data_len);
 
-	memory_pool *mp = get_singleton_mp();
-	unsigned char *buffer = mp_malloc(mp, HUFFMAN_META_DATA_SIZE + sp->data_len * 4);
-
-	int decoded_len = sftt_packet_decode(sp->content, sp->data_len, buffer, true, true);
-	memcpy(sp->content, buffer, decoded_len);
-	sp->data_len = decoded_len;
-
 	if (strcmp(sp->type, BLOCK_TYPE_FILE_NAME) == 0) {
 		printf("decrypted file name: %s\n", sp->content);
 	}
 
-	mp_free(mp, buffer);
+	return 0;
 }
 
 int recv_sftt_packet(int sock, sftt_packet *sp) {
+	sftt_packet *_sp = malloc_sftt_packet(sp->block_size * 2); 
+
 	sftt_packet_recv_header(sock, sp);
 	sftt_packet_recv_content(sock, sp);
+
+	sftt_packet_decode(_sp, sp);
+
+	free_sftt_packet(&_sp);	
 		
 	return 0;
 } 
 
 void free_sftt_packet(sftt_packet **sp) {
+	memory_pool *mp = get_singleton_mp();
+
 	if (sp && *sp) {
-		free((*sp)->content);
-		free(*sp);
+		mp_free(mp, (*sp)->content);
+		mp_free(mp, *sp);
 		*sp = NULL;
 	} 
 }
