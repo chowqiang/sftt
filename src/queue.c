7c905d09d48949dec3227f09f1502cd89920e3e7
zhoumin <zhoumin@bupt.cn>
2022-01-02 19:20:47 +0800

    add debug trace for queue
    Signed-off-by: zhoumin <zhoumin@bupt.cn>

--- src/queue.c
+++ src/queue.c
@@ -17,6 +17,7 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include "debug.h"
 #include "mem_pool.h"
 #include "queue.h"
 #include "show.h"
@@ -25,78 +26,104 @@ extern struct mem_pool *g_mp;
 
 struct queue *queue_create(enum free_mode mode)
 {
+	DBUG_ENTER(__func__);
+
 	struct queue *q = (struct queue *)mp_malloc(g_mp, __func__,
 			sizeof(struct queue));
 	if (q == NULL) {
-		return NULL;
+		DBUG_RETURN(NULL);
 	}
 	q->list = dlist_create(mode);
 	if (q->list == NULL) {
 		mp_free(g_mp, q);
-		return NULL;
+		DBUG_RETURN(NULL);
 	}
 
-	return q;
+	DBUG_RETURN(q);
 }
 
 void queue_init(struct queue *q, enum free_mode mode)
 {
+	DBUG_ENTER(__func__);
+
 	assert(q != NULL);
 	q->list = dlist_create(mode);
+
+	DBUG_VOID_RETURN;
 }
 
 void queue_destroy(struct queue *q)
 {
+	DBUG_ENTER(__func__);
+
 	assert(q != NULL);
-	dlist_destroy(q->list);	
+	dlist_destroy(q->list);
 	q->list = NULL;
 	mp_free(g_mp, q);
+
+	DBUG_VOID_RETURN;
 }
 
 int queue_enqueue(struct queue *q, void *data)
 {
+	DBUG_ENTER(__func__);
+
 	assert(q != NULL);
 
-	return dlist_append(q->list, data);
+	DBUG_RETURN(dlist_append(q->list, data));
 }
 
 int queue_dequeue(struct queue *q, void **data)
 {
+	DBUG_ENTER(__func__);
+
 	assert(q != NULL);
 
-	return dlist_pop_front(q->list, data);
+	DBUG_RETURN(dlist_pop_front(q->list, data));
 }
 
 void *queue_peek(struct queue *q)
 {
+	DBUG_ENTER(__func__);
+
 	assert(q != NULL);
 	struct dlist_node *head = dlist_head(q->list);
 
-	return head == NULL ? NULL : dlist_data(head);
+	DBUG_RETURN(head == NULL ? NULL : dlist_data(head));
 }
 
 int queue_size(struct queue *q)
 {
+	DBUG_ENTER(__func__);
+
 	assert(q != NULL);
 
-	return dlist_size(q->list);
+	DBUG_RETURN(dlist_size(q->list));
 }
 
 void queue_show(struct queue *q, void (*show)(void *data))
 {
+	DBUG_ENTER(__func__);
+
 	assert(q != NULL);
-	
+
 	dlist_set_show(q->list, show);
 	dlist_show(q->list);
+
+	DBUG_VOID_RETURN;
 }
 
 int queue_is_empty(struct queue *q)
 {
-	return q == NULL || q->list == NULL ? 0 : dlist_empty(q->list);
+	DBUG_ENTER(__func__);
+
+	DBUG_RETURN(q == NULL || q->list == NULL ? 0 : dlist_empty(q->list));
 }
 
 int queue_test(void)
 {
+	DBUG_ENTER(__func__);
+
 	struct queue *q = queue_create(FREE_MODE_NOTHING);
 
 	queue_enqueue(q, (void *)1);
@@ -113,10 +140,10 @@ int queue_test(void)
 	queue_dequeue(q, &data);
 	printf("%ld\n", (unsigned long)data);
 	queue_show(q, show_int);
-	
+
 	queue_dequeue(q, &data);
 	printf("%ld\n", (unsigned long)data);
 	queue_show(q, show_int);
-		
-	return 0;
+
+	DBUG_RETURN(0);
 }
